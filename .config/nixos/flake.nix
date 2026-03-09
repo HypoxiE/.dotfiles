9179acf859dd3fd514c61f780494968a099b1aff
HypoxiE <kosmaer42@gmail.com>
2026-03-09 16:45:23 +0300

    added stylix

--- .config/nixos/flake.nix
+++ .config/nixos/flake.nix
@@ -39,7 +39,6 @@
 					home-manager.nixosModules.home-manager
 					{ _module.args.host = "hypoxlaptop"; }
 					{
-						home-manager.useGlobalPkgs = true;
 						home-manager.useUserPackages = true;
 
 						home-manager.sharedModules = [
