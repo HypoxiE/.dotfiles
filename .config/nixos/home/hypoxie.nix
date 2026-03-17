0f6fac1002855ab7a8eebe68bd7500cbcdcc57cb
HypoxiE <kosmaer42@gmail.com>
2026-03-17 16:03:52 +0300

    formatted config

--- .config/nixos/home/hypoxie.nix
+++ .config/nixos/home/hypoxie.nix
@@ -4,58 +4,58 @@ let
 	spicePkgs = spicetify-nix.legacyPackages.${pkgs.stdenv.hostPlatform.system};
 
 	catgirl-downloader =
-	let
-		pythonEnv = pkgs.python3.withPackages (ps: with ps; [
-			pygobject3
-			requests
-		]);
-	in
-		pkgs.stdenv.mkDerivation rec {
-		pname = "catgirl-downloader";
-		version = "0.0.0";
+		let
+			pythonEnv = pkgs.python3.withPackages (ps: with ps; [
+				pygobject3
+				requests
+			]);
+		in
+			pkgs.stdenv.mkDerivation rec {
+				pname = "catgirl-downloader";
+				version = "0.0.0";
 
-		src = pkgs.fetchgit {
-			url = "https://github.com/NyarchLinux/CatgirlDownloader";
-			rev = "6793a5ac678397a1d865d4e1d7769f7a7024ae0c";
-			hash = "sha256-+RyQOgqPZN3AnVdd5mtgppQ/z51VIEeEsiW2RFTnVbk=";
-		};
+				src = pkgs.fetchgit {
+					url = "https://github.com/NyarchLinux/CatgirlDownloader";
+					rev = "6793a5ac678397a1d865d4e1d7769f7a7024ae0c";
+					hash = "sha256-+RyQOgqPZN3AnVdd5mtgppQ/z51VIEeEsiW2RFTnVbk=";
+				};
 
-		nativeBuildInputs = with pkgs; [
-			meson
-			ninja
-			pkg-config
-			gettext
-			wrapGAppsHook3
-			desktop-file-utils
-		appstream-glib
-		];
+				nativeBuildInputs = with pkgs; [
+					meson
+					ninja
+					pkg-config
+					gettext
+					wrapGAppsHook3
+					desktop-file-utils
+				appstream-glib
+				];
 
-		buildInputs = with pkgs; [
-			pythonEnv
-			gtk4
-			glib
-			gobject-introspection
-			libadwaita
-		];
+				buildInputs = with pkgs; [
+					pythonEnv
+					gtk4
+					glib
+					gobject-introspection
+					libadwaita
+				];
 
-		configurePhase = ''
-			meson setup build --prefix=$out -Dauto_features=enabled -Dwrap_mode=nodownload
-		'';
+				configurePhase = ''
+					meson setup build --prefix=$out -Dauto_features=enabled -Dwrap_mode=nodownload
+				'';
 
-		buildPhase = "meson compile -C build";
-		installPhase = "meson install -C build";
+				buildPhase = "meson compile -C build";
+				installPhase = "meson install -C build";
 
-		postInstall = ''
-		wrapProgram $out/bin/catgirldownloader \
-			--set PATH ${pythonEnv}/bin:$PATH \
-			--prefix GI_TYPELIB_PATH : \
-			${pkgs.glib.out}/lib/girepository-1.0 \
-			${pkgs.gtk4.out}/lib/girepository-1.0 \
-			${pkgs.gdk-pixbuf.out}/lib/girepository-1.0 \
-			${pkgs.pango.out}/lib/girepository-1.0 \
-			${pkgs.harfbuzz.out}/lib/girepository-1.0
-		'';
-		};
+				postInstall = ''
+				wrapProgram $out/bin/catgirldownloader \
+					--set PATH ${pythonEnv}/bin:$PATH \
+					--prefix GI_TYPELIB_PATH : \
+					${pkgs.glib.out}/lib/girepository-1.0 \
+					${pkgs.gtk4.out}/lib/girepository-1.0 \
+					${pkgs.gdk-pixbuf.out}/lib/girepository-1.0 \
+					${pkgs.pango.out}/lib/girepository-1.0 \
+					${pkgs.harfbuzz.out}/lib/girepository-1.0
+				'';
+			};
 
 	hyprmodify = pkgs.buildGoModule {
 		pname = "hyprmodify";
