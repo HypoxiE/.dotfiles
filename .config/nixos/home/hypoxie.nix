4925610d7378e877d438cade87fa153730a6dd16
HypoxiE <kosmaer42@gmail.com>
2026-03-22 13:26:53 +0300

    cpp now workssudo!

--- .config/nixos/home/hypoxie.nix
+++ .config/nixos/home/hypoxie.nix
@@ -234,6 +234,38 @@ in
 		img = "chafa";
 		};
 	};
+
+	programs.vscode = {
+		enable = true;
+		package = pkgs.vscode.fhs;
+
+		extensions = with pkgs.vscode-extensions; [
+			bbenoist.nix
+			ms-vscode.cpptools
+			ms-vscode.cmake-tools
+
+			rust-lang.rust-analyzer
+			golang.go
+
+			ms-python.python
+			james-yu.latex-workshop
+		]++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+			{
+				name = "save-as-root";
+				publisher = "yy0931";
+				version = "1.12.0";
+				sha256 = "fGYqT7emOL14p3LfAaR4CaxUkTYHbopIOc25TC248r4=";
+			}
+			{
+				name = "yuck";
+				publisher = "eww-yuck";
+				version = "0.0.3";
+				sha256 = "DITgLedaO0Ifrttu+ZXkiaVA7Ua5RXc4jXQHPYLqrcM=";
+			}
+
+		];
+	};
+
 	gtk = {
 		enable = true;
 		iconTheme = {
@@ -314,6 +346,9 @@ in
 		android-tools
 		mtkclient
 		bruno # http requests
+		gcc
+		gdb
+		cmake
 
 		#vtubing
 		inochi-session
@@ -322,78 +357,5 @@ in
 
 		#ssh
 		gnupg
-
-		(vscode-with-extensions.override {
-		vscodeExtensions = with vscode-extensions; [
-			bbenoist.nix
-		] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-			{
-				name = "save-as-root";
-				publisher = "yy0931";
-				version = "1.12.0";
-				sha256 = "fGYqT7emOL14p3LfAaR4CaxUkTYHbopIOc25TC248r4=";
-			}
-			{
-				name = "latex-workshop";
-				publisher = "james-yu";
-				version = "10.12.2";
-				sha256 = "6VXlsMtAPFROYlmYJdHj54fo1J0LC4UJbzI00cuuwhk=";
-			}
-			{
-				name = "yuck";
-				publisher = "eww-yuck";
-				version = "0.0.3";
-				sha256 = "DITgLedaO0Ifrttu+ZXkiaVA7Ua5RXc4jXQHPYLqrcM=";
-			}
-			{
-				name = "cpptools-extension-pack";
-				publisher = "ms-vscode";
-				version = "1.3.1";
-				sha256 = "HbI0UdN8uwHS2MPH1SGZhxNaN18cWzjMyWYcgVE7FjY=";
-			}
-			{
-				name = "cpptools";
-				publisher = "ms-vscode";
-				version = "1.29.3";
-				sha256 = "wDHdpW6vV183RrMrssCGFW6w5IrqnlvIZO1dr5T6Syg=";
-			}
-			{
-				name = "cpptools-themes";
-				publisher = "ms-vscode";
-				version = "2.0.0";
-				sha256 = "YWA5UsA+cgvI66uB9d9smwghmsqf3vZPFNpSCK+DJxc=";
-			}
-			{
-				name = "cpp-devtools";
-				publisher = "ms-vscode";
-				version = "0.2.0";
-				sha256 = "cTUwhnwZ51gh89mLoNI/mVITor2KbZrhCG9M8gHbOjc=";
-			}
-			{
-				name = "cmake-tools";
-				publisher = "ms-vscode";
-				version = "1.22.27";
-				sha256 = "pMAJ2pk32KX5vc3QUqXIRTlBQuvOR0EiHpIfQe5aTwU=";
-			}
-			{
-				name = "rust-analyzer";
-				publisher = "rust-lang";
-				version = "0.3.2743";
-				sha256 = "rQO6suFU81fEzMCGzjugxnj8hs/Xv+26gAPQqwmaBts=";
-			}
-			{
-				name = "python";
-				publisher = "ms-python";
-				version = "2026.0.0";
-				sha256 = "se9kL7KmBZSh6R5f91XKey3CWLQVQNtEEbaHQ4AZAuo=";
-			}
-			{
-				name = "go";
-				publisher = "golang";
-				version = "0.52.2";
-				sha256 = "8g+r4Mv06Bx1W3yAXWVbtz1B/gXPcRdmaV0tPkTP6Gk=";
-			}
-		];
-		})
 	];
 }
--- .config/nixos/hosts/configuration.nix
+++ .config/nixos/hosts/configuration.nix
@@ -215,6 +215,8 @@ in
 	hardware.bluetooth.enable = true;
 	hardware.bluetooth.powerOnBoot = true;
 
+	#programs.nix-ld.enable = true;
+
 	# List packages installed in system profile.
 	# You can use https://search.nixos.org/ to find more packages (and options).
 	environment.systemPackages = with pkgs; [
