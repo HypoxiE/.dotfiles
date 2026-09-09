5fd2738b58d2a6f4583e073b439fa6fa52a60799
HypoxiE <kosmaer42@gmail.com>
2026-09-09 11:59:46 +0700

    deleted nvidia drivers

--- .config/nixos/hosts/specific/specific-hynix.nix
+++ .config/nixos/hosts/specific/specific-hynix.nix
@@ -1,37 +1,31 @@
-{ config, lib, pkgs, host, ... }:
-
+{
+  config,
+  lib,
+  pkgs,
+  host,
+  ...
+}:
 lib.mkIf (host == "hynix") {
+  swapDevices = [
+    {
+      device = "/swap/swapfile";
+      size = 32022;
+      priority = -300;
+    }
+  ];
+  # services.xserver.videoDrivers = [ ];
+  # boot.initrd.kernelModules = [ "vfio_pci" "vfio" "vfio_iommu_type1" ];
+  # boot.kernelParams = [
+  #     "iommu=pt"
+  #     "pcie_acs_override=downstream"
+  #     "vfio-pci.ids=10de:1f0a,10de:10f9"
+  # ];
+  # boot.blacklistedKernelModules = [
+  #     "nouveau"
+  #     "nvidia"
+  #     "nvidia_drm"
+  #     "nvidia_modset"
+  # ];
 
-	swapDevices = [
-		{
-			device = "/swap/swapfile";
-			size = 32022;
-			priority = -300;
-		}
-	];
-    # services.xserver.videoDrivers = [ ];
-    # boot.initrd.kernelModules = [ "vfio_pci" "vfio" "vfio_iommu_type1" ];
-    # boot.kernelParams = [
-    #     "iommu=pt"
-    #     "pcie_acs_override=downstream"
-    #     "vfio-pci.ids=10de:1f0a,10de:10f9"
-    # ];
-    # boot.blacklistedKernelModules = [
-    #     "nouveau"
-    #     "nvidia"
-    #     "nvidia_drm"
-    #     "nvidia_modset"
-    # ];
-
-	boot.extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
-	hardware.graphics.enable = true;
-	services.xserver.videoDrivers = [ "nvidia" ];
-	hardware.nvidia = {
-		modesetting.enable = true;
-		powerManagement.finegrained = false;
-		open = false;
-		nvidiaSettings = true;
-		package = config.boot.kernelPackages.nvidiaPackages.stable;
-		prime.nvidiaBusId = "PCI:01:00.0";
-	};
+  #hardware.graphics.enable = true;
 }
